bc7260939c4ada7b1ddd1ef86bf5b32556371ac6
kewton <newtons.boiled.clock@gmail.com>
2024-12-31 14:56:32 +0900

    モデルを最新化

--- initdatabase.py
+++ initdatabase.py
@@ -7,12 +7,10 @@ def initdata():
         "gpt",
         {
             "gpt_model": [
-                "gpt-4o",
-                "gpt-4",
-                "gpt-3.5-turbo",
-                "gpt-4-32k",
-                "gpt-4-turbo",
-                "claude-3-opus-20240229",
+                "chatgpt-4o-latest",
+                "gpt-4o-mini",
+                "o1-mini",
+                "o1-preview",
                 "claude-3-sonnet-20240229",
                 "claude-3-haiku-20240307"
             ]
@@ -23,7 +21,7 @@ def initdata():
         "chat",
         "systemrole",
         {
-            "プログラミング言語": ["Next.js_1", "Next.js_2"],
+            "プログラミング言語": ["FastAPI", "Next.js_1", "Next.js_2"],
             "Next.js_1": {
                 "srcdire": "src",
                 "libraryFileList": [
@@ -47,6 +45,22 @@ def initdata():
                     ".DS_Store"
                 ],
                 "prompt": "nextjstemplate2"
+            },
+            "FastAPI": {
+                "srcdire": "../test",
+                "libraryFileList": [
+                    "requirements.txt"
+                ],
+                "prerequisites": "./docs/requiredSpecifications.md に記載された要求仕様書に従ったFastAPIのバックエンドエンドAPIを開発しています。利用者に最高のユーザー体験を提供します。",
+                "system_role": "あなたは優秀なFastAPIのバックエンドエンジニアです。入力された情報を元に最高のコードをアウトプットします。",
+                "ignorelist": [
+                    "__pycache__/",
+                    "venv/",
+                    "*.db",
+                    "*.DS_Store",
+                    ".log"
+                ],
+                "prompt": "fastAPItemplate"
             }
         }
     )
